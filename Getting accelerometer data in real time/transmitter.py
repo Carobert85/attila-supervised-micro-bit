0d09c923c721b8e02236bfb89919559ebbab2976
Attila Teiermayer <teiermayer.attila@gmail.com>
2020-12-11 18:25:14 +0000

    Create transmitter.py

---
+++ Getting accelerometer data in real time/transmitter.py
@@ -0,0 +1,11 @@
+from microbit import*
+import radio
+
+#configuring radio
+radio.on()
+radio.config(power = 7, group = 1)
+
+#getting the data from the accelerometer and send it via radio
+sleep_ms(1)
+accel = accelerometer.get_z() # get the data from the z-axis
+radio.send(str(accel))
