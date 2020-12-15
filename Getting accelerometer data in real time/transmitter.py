828fabdee02224be711fd45be0c1d39709d73873
Attila Teiermayer <teiermayer.attila@gmail.com>
2020-12-15 20:37:34 +0000

    Update transmitter.py

--- Getting accelerometer data in real time/transmitter.py
+++ Getting accelerometer data in real time/transmitter.py
@@ -1,11 +1,12 @@
 from microbit import*
-import radio
+import radio, utime
 
 #configuring radio
 radio.on()
 radio.config(power = 7, group = 1)
 
 #getting the data from the accelerometer and send it via radio
-sleep_ms(1)
-accel = accelerometer.get_z() # get the data from the z-axis
-radio.send(str(accel))
+while True:
+  utime.sleep_ms(1)
+  accel = accelerometer.get_z() # get the data from the z-axis
+  radio.send(str(accel))
