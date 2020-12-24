2485cd01f74e089082ba5a1edd1313d7f2c7c778
Attila Teiermayer <teiermayer.attila@gmail.com>
2020-12-24 12:02:33 +0000

    Update transmitter.py

--- Getting accelerometer data in real time/transmitter.py
+++ Getting accelerometer data in real time/transmitter.py
@@ -7,6 +7,6 @@ radio.config(power = 7, group = 1)
 
 #getting the data from the accelerometer and send it via radio
 while True:
-  utime.sleep_ms(1)
+  utime.sleep_ms(10)
   accel = accelerometer.get_z() # get the data from the z-axis
-  radio.send(str(accel))
+  radio.send(str(accel)) # data type of sent message must be string
