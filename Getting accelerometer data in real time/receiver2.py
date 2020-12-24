1de5781c72ec3d751490237561f4f2266e569204
Attila Teiermayer <teiermayer.attila@gmail.com>
2020-12-24 12:00:34 +0000

    Rename receiver.py to receiver2.py

--- Getting accelerometer data in real time/receiver2.py
+++ Getting accelerometer data in real time/receiver2.py
@@ -6,12 +6,17 @@ radio.on()
 radio.config(group = 1)
 
 while True:
-    message = None
-    while message is None:
-        try:
-            message = radio.receive()
-            utime.sleep_ms(1)
-            acc_z = int(message)
-            print(acc_z)
-        except ValueError:
-            print('Cannot do it')
+    message = radio.receive()
+    utime.sleep_ms(100)
+    # Often occurs that transmitter device stops working due to the battery issues.
+    #So, we have to check if the connection is living with the next snippet of code.
+    # If there is connection, the data type of the received message is 'str', 'None' otherwise.
+    
+    if type(message) == str:
+        accel_z = int(message)
+        accel_z = (9.81/1024)*accel_z # turn the number provided by the AD converter to the real value of acceleration in m/s**2
+        to_print = (0,0,accel_z) # set up a tuple to help the plotter of Mu to work
+        print(to_print)
+        
+    else:
+        print('I lost the connection with the transmitter')
