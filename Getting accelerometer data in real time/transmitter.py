3a905099f239edc1152673f8b9482c03c15b903a
Attila Teiermayer <teiermayer.attila@gmail.com>
2021-10-20 15:07:32 +0100

    Update transmitter.py

--- Getting accelerometer data in real time/transmitter.py
+++ Getting accelerometer data in real time/transmitter.py
@@ -2,14 +2,14 @@ from microbit import*
 import radio
 import utime
 
-#configuring radio
+#Set up radio connection and configuring radio network
 radio.on()
 radio.config(power = 7, group = 1)
 
 #getting the data from the accelerometer and send it via radio
 #I convert the digital data provided by the device into real acceleration values and round it to floats with 2 decimal places 
 while True:
-    utime.sleep_ms(1)
+    utime.sleep_ms(1) # the data is collected in every second millisecond
     accel_x = round((9.81/1024)*accelerometer.get_x(), 2) # get and convert the data along the x-axis
     accel_y = round((9.81/1024)*accelerometer.get_y(), 2) # get and convert the data along the y-axis
     accel_z = round((9.81/1024)*accelerometer.get_z(), 2) # get and convert the data along the z-axis
