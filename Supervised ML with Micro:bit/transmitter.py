213264440fc063f54d59b1340eb68aa93a194244
Attila Teiermayer <teiermayer.attila@gmail.com>
2021-12-09 15:09:43 +0000

    Create transmitter.py

---
+++ Supervised ML with Micro:bit/transmitter.py
@@ -0,0 +1,40 @@
+from microbit import*
+import radio
+import utime
+import math
+
+#configuring radio
+radio.on()
+radio.config(power = 7, group = 1)
+
+# the data acquisition lasts for 16 seconds, so I get the start time from the processor's running time
+start = running_time()
+
+radio.send("0, 0, 0") # this is the id of the motion. 0 stands for SHM, 1 for pendulum and 2 for circular motion
+
+counter = 0 # I define this variable to count the iterations whilst collecting the data. counter is going to be sent to the main computer to help reading the data
+
+# the data acquisition process
+while True:
+    time = running_time()-start # I define time of data collection using the processor's running time
+    if time <16000: 
+        utime.sleep_ms(50) # the data is collected in every 50 milliseconds
+        
+        # getting the digital data along x and y axes and converting them into real acceleration values. Working out the value of rotion as well
+        accel_x = round((9.81/1024)*accelerometer.get_x(), 2) # acceleartion along the x-axis
+        accel_y = round((9.81/1024)*accelerometer.get_y(), 2) # acceleration along the y-axis
+        angle = round(math.atan2(accel_x, accel_y),2) # work out the angle of rotation
+        
+        # sending the data via radio
+        send = (accel_x, accel_y, angle) # the tuple that is going to be sent to the main computer
+        radio.send(str(counter))
+        radio.send(str(send))
+        counter+=1
+    else:
+        radio.off()
+        while True:
+            display.scroll("restart me")
+
+
+
+
